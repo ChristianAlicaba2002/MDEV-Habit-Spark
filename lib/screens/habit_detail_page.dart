2af9ce2627dd1f70258a250638197ecae2ecab2e
Davefgh <saysonchan01@gmail.com>
2026-04-02 08:48:45 +0800

    fix: Add missing dart:io import for File class

--- lib/screens/habit_detail_page.dart
+++ lib/screens/habit_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:habit_spark/models/habit.dart';
